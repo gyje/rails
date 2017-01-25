38c1badd6c7f7514fee234460f7329aaccd12f64
pushring0 <gyj37191@gmail.com>
2017-01-25 18:38:03 +0800

    python test

--- printtab.py
+++ printtab.py
@@ -1,9 +1,11 @@
-def print_tab(the_list):
+def print_tab(the_list,level=0):
 	for each_item in the_list:
 		if isinstance(each_item,list):
-			print_tab(each_item)
+			print_tab(each_item,level+1)
 		else:
+			for i in each_item:
+				print ("\t")
 			print(each_item)
 
 list1=["1","2",'3','test',[1,2,3],100*6]		
-print_tab(list1)	
+print_tab(list1,2)	
